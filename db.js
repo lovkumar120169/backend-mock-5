5e27e7be0130c6077855283b96e633f1cb2e290f
unknown <lovkumar120169@gmail.com>
2023-08-21 18:45:32 +0530

    changes

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
 
-const connection=mongoose.connect("mongodb+srv://luv:kumar@cluster0.e5ouqwp.mongodb.net/mockuser?retryWrites=true&w=majority")
+const connection=mongoose.connect("mongodb+srv://luv:kumar@cluster0.e5ouqwp.mongodb.net/mockuser")
 
 module.exports={connection}
